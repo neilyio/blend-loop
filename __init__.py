3f02d361e2a4ea01f5fc431dae2f8398167694c8
Neil Hansen <neil.hansen.31@gmail.com>
2020-05-26 19:34:54 -0700

    Add license and bl_info.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,32 @@
+'''
+Copyright (C) 2020 Neil Hansen
+http://neily.io
+neil.hansen.31@gmail.com
+Created by Neil Hansen
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+bl_info = {
+    "name":        "Blend Loop",
+    "description": "Automatically run external Python scripts in Blender.",
+    "author":      "Neil Hansen",
+    "version":     (0, 1, 0),
+    "blender":     (2, 80, 0),
+    "location":    "View 3D > Properties > Blend Loop",
+    "tracker_url": "",
+    "category":    "Development"
+}
+
+
 import bpy
 import sys
 from pathlib import Path
